27cc0269f0c6c0209e429840f6a926836b6f6417
Michael Gibney <michaelgibney1@gmail.com>
2022-07-27 16:30:12 -0700

    updated rhel

---
+++ configs/halo/rhel/ansible/rhelScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+touch check.txt
---
+++ configs/halo/rhel/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SET CREDENTIAL ENV VARIABLES FROM INPUT
+echo "Enter aws access key:"
+read aws_key
+
+echo "Enter aws secret key:"
+read aws_secret
+
+export AWS_ACCESS_KEY="$aws_key"
+export AWS_SECRET_KEY="$aws_secret"
+
+
+# EXECUTE PACKER BUILD
+packer build -only=amazon-ebs -var-file=rhelVars.json \
+-var "aws_region=us-east-1" -var "description=rhelGoldenImage" -var "version=0.1" \
+config.json
+
+# packer build -only=azure-rm,docker config.json
