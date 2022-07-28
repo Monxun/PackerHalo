3e0346d480d10148793a7072f61802586307be74
Michael Gibney <michaelgibney1@gmail.com>
2022-07-27 18:44:50 -0700

    script

--- configs/halo/initHaloPacker.sh
+++ configs/halo/initHaloPacker.sh
@@ -6,10 +6,11 @@ sudo yum update
 # INSTALL PYTHON3
 sudo yum search python3
 sudo yum install python3 -y
-sudo yum install ansible
 
 # INSTALL ANSIBLE
-sudo python3 -m pip install --user ansible
+sudo yum -y install epel-repo
+sudo yum update
+sudo yum -y install ansible
 
 # DOWNLOAD CIS ANSIBLE ROLE
 ansible-galaxy install RedHatOfficial.rhel8_cis
