d218caf4f6998ec0af25dc69dc718eb79061826d
Michael Gibney <michaelgibney1@gmail.com>
2022-07-27 18:59:00 -0700

    script

--- configs/halo/initHaloPacker.sh
+++ configs/halo/initHaloPacker.sh
@@ -7,7 +7,7 @@ sudo yum update
 sudo subscription-manager repos --enable rhel-*-optional-rpms \
                            --enable rhel-*-extras-rpms \
                            --enable rhel-ha-for-rhel-*-server-rpms
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum update
 sudo yum install ansible
 ansible --version
