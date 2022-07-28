6a4679c4a77eefde4b4a467834ac344f7889ba31
Michael Gibney <michaelgibney1@gmail.com>
2022-07-27 19:11:15 -0700

    scripts

--- configs/halo/rhel/build.sh
+++ configs/halo/rhel/build.sh
@@ -12,6 +12,8 @@ export AWS_SECRET_KEY="$aws_secret"
 
 
 # EXECUTE PACKER BUILD
-packer build -only=amazon-ebs -var-file=rhelVars.json rhelConfig.json
+packer build -only=amazon-ebs -var-file=rhelVars.json \
+-var "aws_region=us-east-1" -var "aws_description=rhelGoldenImage" -var "aws_version=0.1" \
+rhelConfig.json
 
 # packer build -only=azure-rm,docker config.json
