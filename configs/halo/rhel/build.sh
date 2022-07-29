980d4d22e741bad1f20c4e03811b0d148085b8f1
Michael Gibney <michaelgibney1@gmail.com>
2022-07-29 11:24:23 -0700

    scripts

--- configs/halo/rhel/build.sh
+++ configs/halo/rhel/build.sh
@@ -26,9 +26,9 @@ fi
 # EXECUTE PACKER BUILD
 packer init rhel.pkr.hcl
 packer build -only=amazon-ebs -var-file=rhelVars.json \
--var "aws_region=us-gov-west-1" -var "aws_version=0.1" \
+# -var "aws_region=us-gov-west-1" -var "aws_version=0.1" \
 rhelConfig.json
 
-aws ec2 describe-images --region us-west-gov-1
+aws ec2 describe-images --region us-gov-west-1
 
 # packer build -only=azure-rm,docker config.json
