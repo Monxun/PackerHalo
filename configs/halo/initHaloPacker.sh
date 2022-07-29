dd251b6af402d4b1d87ba2e70f27f703b0bf05b3
Michael Gibney <michaelgibney1@gmail.com>
2022-07-29 13:03:07 -0700

    script jinja2

--- configs/halo/initHaloPacker.sh
+++ configs/halo/initHaloPacker.sh
@@ -3,16 +3,8 @@
 # UPDATE
 sudo yum update
 
-# INSTALL AWS CLI
-sudo yum -y install unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-
-
-
 # INSTALL ANSIBLE
+pip install jinja2
 sudo subscription-manager repos --enable rhel-*-optional-rpms \
                            --enable rhel-*-extras-rpms \
                            --enable rhel-ha-for-rhel-*-server-rpms
@@ -24,3 +16,9 @@ ansible --version
 # DOWNLOAD CIS ANSIBLE ROLE
 ansible-galaxy install mindpointgroup.rhel7_cis
 ansible-galaxy install RedHatOfficial.rhel8_cis
+
+# INSTALL AWS CLI
+sudo yum -y install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
