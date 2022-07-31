9e7ee69c5a3138eef38b6419db818209ed16aaac
Michael Gibney <michaelgibney1@gmail.com>
2022-07-31 15:44:06 -0700

    s

--- configs/ansible/roles/rhel/files/fips.sh
+++ configs/ansible/roles/rhel/files/fips.sh
@@ -15,7 +15,7 @@ rpm -q prelink && prelink -uav
 mv -v /boot/initramfs-$(uname -r).img{,.bak}
 dracut
 grubby --update-kernel=$(grubby --default-kernel) --args=fips=1
-uuid=$(findmnt -no uuid /boot)
+uuid=$(findmnt --output=UUID --noheadings --target=/boot)
 [[ -n $uuid ]] && grubby --update-kernel=$(grubby --default-kernel) --args=boot=UUID=${uuid}
 EOF
 
