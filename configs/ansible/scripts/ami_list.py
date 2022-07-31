4b9003658b73866a0465a768fb9fb9e9074a3bbf
Michael Gibney <michaelgibney1@gmail.com>
2022-07-31 13:38:40 -0700

    python

--- configs/ansible/scripts/ami_list.py
+++ configs/ansible/scripts/ami_list.py
@@ -3,6 +3,6 @@ import boto3
 
 ec2_client = boto3.client('ec2', region_name='us-gov-west-1') # Change as appropriate
 images = ec2_client.describe_images(Owners=['self'])
-ami_list = [image for image in images['Images']]
+ami_list = [(image['ImageLocation'], image['ImageId']) for image in images['Images']]
 # print(json.dumps(images, indent=4, sort_keys=True))
 print(json.dumps(ami_list, indent=4, sort_keys=True))
